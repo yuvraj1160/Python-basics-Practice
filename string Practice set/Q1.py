8a61f1d75624ff066350aac6055322999521c38b
yuvraj1160 <yuvrathod1610@gmail.com>
2026-06-08 21:58:41 +0530

    Add question

--- string Practice set/Q1.py
+++ string Practice set/Q1.py
@@ -1,3 +1,9 @@
+'''Q1 -> Write a python program to display a user entered name followed by Good Afternoon using
+input() function. '''
+
+
+# Code 
+
 name = input ("enter your name : " )
 
 print("Good Afternoon",name )
