512fc3d8b654f4aefec209569ff62846c638ba01
yuvraj1160 <yuvrathod1610@gmail.com>
2026-06-17 23:51:33 +0530

    Dictionar question 1

---
+++ Dictionary/Q1.py
@@ -0,0 +1,13 @@
+""" Question 1 -> Write a program to create a dictionary of Hindi words with values as their English
+translation. Provide user with an option to look it up! """
+
+# Code 
+
+words = {
+    "madad":"help",
+    "kyu":"why",
+    "kursi":"Chair"
+}
+word = input ("enter a word u want : ")
+
+print(words[word])
