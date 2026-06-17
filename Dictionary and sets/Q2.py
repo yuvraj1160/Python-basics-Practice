2cd7c766367c2e692b1ee0f706e8e9222f03b904
yuvraj1160 <yuvrathod1610@gmail.com>
2026-06-17 23:54:25 +0530

    sets question added

---
+++ Dictionary and sets/Q2.py
@@ -0,0 +1,24 @@
+""" Question 2 -> Write a program to input eight numbers from the user and display all the unique numbers
+(once). """
+
+# CODE ->
+
+s=set()
+n =int(input("enter a no. : "))
+s.add(n)
+n =int(input("enter a no. : "))
+s.add(n)
+n =int(input("enter a no. : "))
+s.add(n)
+n =int(input("enter a no. : "))
+s.add(n)
+n =int(input("enter a no. : "))
+s.add(n)
+n =int(input("enter a no. : "))
+s.add(n)
+n =int(input("enter a no. : "))
+s.add(n)
+n =int(input("enter a no. : "))
+s.add(n)
+
+print (s)   
